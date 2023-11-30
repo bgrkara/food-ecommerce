8ec83ca82b8a72632b91a738be68db468113cff2
Buğra Kara <bgrkara@gmail.com>
2023-11-30 15:02:02 +0300

    Ürün Kategorileri Bölümü

---
+++ resources/views/frontend/home/component/promo_first.blade.php
@@ -0,0 +1,19 @@
+<div class="ps-promo">
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <div class="ps-promo__item"><img class="ps-promo__banner" src="{{ asset('frontend/img/promotion/bg-banner4.jpg')}}" alt="alt" />
+                <div class="ps-promo__content"><span class="ps-promo__badge">New</span>
+                    <h4 class="mb-20 ps-promo__name">Get rid of bacteria <br/>in your home</h4><a class="ps-promo__btn" href="category-grid.html">More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="ps-promo__item"><img class="ps-promo__banner" src="{{ asset('frontend/img/promotion/bg-banner5.jpg')}}" alt="alt" />
+                <div class="ps-promo__content">
+                    <h4 class="ps-promo__name">Candid <br/>Whitening Kit</h4>
+                    <div class="ps-promo__sale">-10%</div><a class="ps-promo__btn" href="category-grid.html">Shop now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
